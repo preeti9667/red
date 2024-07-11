b969be4db26acc40f9050d188522159f7531550a
preeti9667 <saini04preeti@gmail.com>
2024-07-11 15:43:41 +0530

    fixed file

--- src/app/offers/page.js
+++ src/app/offers/page.js
@@ -1,8 +1,12 @@
+import Link from "next/link";
 
 export default function offers() {
     return (
      <>
      <h1>hello offers</h1>
+     {/* <button>
+      <Link href='../search'></Link>
+      click</button> */}
      </>
     );
   }
