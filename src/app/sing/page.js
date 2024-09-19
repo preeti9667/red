a7c7897665f0d75b9f8e43c7da2575b65505def1
preeti9667 <saini04preeti@gmail.com>
2024-09-19 11:29:25 +0530

    add more text

---
+++ src/app/sing/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Sign = () => {
+  return (
+    <div>
+      <h1>hello  </h1>
+      <div>
+        this is a ne content i am  write a page 
+      </div>
+      <div>this website is a good </div>
+    </div>
+  )
+}
+
+export default Sign
